5248444f67e88e75bee8f01f7f73235a9a22a9f6
exchron0s <mega.nemirovskiy@gmail.com>
2024-03-29 09:27:25 +0300

    #3.1 and #3.2

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-#1
+import math
+#3.1
 
-string = 'have a nice day'
+num = int(input('Введите число: '))
 
-mas = string.split(' ')
-newmas = mas[::-1]
-newstring = ''
+if num > 0:
+    print('Положительное')
+elif num < 0:
+    print('Отрицательное')
+else:
+    print('Zero')
 
-for i in newmas:
-    newstring += i+' '
+#3.2
+    
+a = 2.5
+x = float(input('Введите число: '))
 
-print(newstring)
+def ret(x, a):
+    y = 0
+
+    if x > a:
+        y = x * math.pow(x-a, 1/3)
+    elif x == a:
+        y = x * math.sin(a*x)
+    else:
+        y = math.exp(-a*x) * math.cos(a*x)
+
+    return y
+
+print(ret(x, a))
