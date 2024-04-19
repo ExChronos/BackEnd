67652860a4442b190956bc0b74b93dd882aaa7ec
Nathan <mega.nemirovskiy@gmail.com>
2024-04-19 09:19:47 +0300

    Task 4 - while cycle

--- main.py
+++ main.py
@@ -1,30 +1,80 @@
-import math
-#3.1
+#4.1
+#сумма чисел в массиве
+def four_point_one():
+    query = [i for i in range(12)]
+    sum = 0
 
-num = int(input('Введите число: '))
-
-if num > 0:
-    print('Положительное')
-elif num < 0:
-    print('Отрицательное')
-else:
-    print('Zero')
-
-#3.2
+    for i in query:
+        sum += i
     
-a = 2.5
-x = float(input('Введите число: '))
+    return sum
 
-def ret(x, a):
-    y = 0
 
-    if x > a:
-        y = x * math.pow(x-a, 1/3)
-    elif x == a:
-        y = x * math.sin(a*x)
-    else:
-        y = math.exp(-a*x) * math.cos(a*x)
+#4.2
+#сумма чисел в трехзначном числе
 
-    return y
+def four_point_two():
+    number = int(input('Введите трехзначное число: '))
+    sum3 = 0
 
-print(ret(x, a))
+    for i in str(number):
+        sum3 += int(i)
+
+    return sum3
+
+#4.3
+#вывод степеней двойки меньше чем N
+
+def four_point_three():
+    N = int(input("Введите число N(Степени 2): "))
+    k = 0
+    num2 = 2**k
+
+    while num2 < N:
+        print(num2)
+        k+=1
+        num2 = 2**k
+
+#4.4
+#вычислить факториал числа N
+
+def four_point_four():
+    N = int(input('Введите число N(Факториал N): '))
+    factor = 1
+    i = 1
+
+    while i <= N:
+        factor *= i
+        i+=1
+    
+    return f'{factor}\n'
+
+#4.5
+#вывести на экран числа кратные 7
+
+def four_point_five():
+    k = 1
+
+    while k <= 14:
+        print(7*k)
+        k+=1
+    print('\n')
+
+#4.6
+#вычислить сумму 5 произвольных чисел
+
+def four_point_six():
+    nums = []
+    print("Сумма 5 чисел\n")
+
+    for k in range(5):
+        nums.append(int(input('Введите число: ')))
+    
+    return sum(nums)
+
+print(four_point_one())
+print(four_point_two())
+four_point_three()
+print(four_point_four())
+four_point_five()
+print(four_point_six())
